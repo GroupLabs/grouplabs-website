61b608f849c4891af49c83c7ce988dc3fc08f2f2
noelfranthomas <noelfranthomas@gmail.com>
2022-09-04 16:53:11 -0600

    Update client.js

--- components/client.js
+++ components/client.js
@@ -5,6 +5,5 @@ export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   apiVersion: '2022-09-04', // use a UTC date string
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   useCdn: true // `false` if you want to ensure fresh data
 })
