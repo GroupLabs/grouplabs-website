26f177f44ee50c9b0352e534be3e66d58efb605f
noelfranthomas <noelfranthomas@gmail.com>
2022-09-04 21:29:31 -0600

    Meta again

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta property="og:title" content="GroupLabs" />
         <meta property="og:type" content="home page" />
         <meta property="og:description" content="Open collaboration on projects. Impact on local communities. Building the future together." />
-        <meta property="og:image" content="https://grouplabs.ca/public/GL.svg" />
+        <meta property="og:image" content="https://grouplabs.ca/GL.svg" />
         <meta property="og:url" content="https://www.grouplabs.ca" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
