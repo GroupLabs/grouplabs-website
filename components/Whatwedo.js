62dc3fe4296f4e29da0c0889f08c720819517622
noelfranthomas <noelfranthomas@gmail.com>
2022-09-07 17:20:56 -0600

    Added for companies, for devs

--- components/Whatwedo.js
+++ components/Whatwedo.js
@@ -10,52 +10,78 @@ export default function WhatWeDo() {
   return (
     <div className={styles.wContainer}>
         <div className={styles.wHeader}>What we do</div>
-        <div className={styles.wElement}>
-            <div className={styles.wElementImage}>
-            <Image
-                src={peer}
-                alt="Picture of the author"
-                width={300}
-                height={200}
-                // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
-            />
+
+        <div className={styles.wContent}>
+            <div>
+                <div className={styles.ContentSubHeader}>For Companies</div>
+                <div className={styles.wElement}>
+                    <div className={styles.wElementImage}>
+                    <Image
+                        src={peer}
+                        alt="Picture of the author"
+                        width={300}
+                        height={200}
+                        // blurDataURL="data:..." automatically provided
+                        // placeholder="blur" // Optional blur-up while loading
+                    />
+                    </div>
+                    <div className={styles.wElementTextContainer}>
+                        <div className={styles.wElementText}>Rapid Onboarding</div>
+                        <div className={styles.wElementSubText}>Need a team for a project? With our integrated onboarding approach this becomes a breeze. Or, incorporate this with your hiring process to help you find the right team for your codebase.</div>
+                    </div>
+                </div>
+                <div className={styles.wElement}>
+                    <div className={styles.wElementImage}>
+                    <Image
+                        src={onboard}
+                        alt="Picture of the author"
+                        width={300}
+                        height={200}
+                        // blurDataURL="data:..." automatically provided
+                        // placeholder="blur" // Optional blur-up while loading
+                    />
+                    </div>
+                    <div className={styles.wElementTextContainer}>
+                        <div className={styles.wElementText}>Secure Modules</div>
+                        <div className={styles.wElementSubText}>We handle code splitting to protect your confidentiality while still being able to tap into our talented team of developers.</div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.wElementTextContainer}>
-                <div className={styles.wElementText}>Peer-to-peer Networking</div>
-                <div className={styles.wElementSubText}>Find like minded individuals in the local community. Engage and collaborate on projects with provided resources. Let&apos;s build something together today.</div>
-            </div>
-        </div>
-        <div className={styles.wElement}>
-            <div className={styles.wElementImage}>
-            <Image
-                src={group}
-                alt="Picture of the author"
-                width={300} 
-                height={200}
-                // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
-            />
-            </div>
-            <div className={styles.wElementTextContainer}>
-                <div className={styles.wElementText}>Group Projects</div>
-                <div className={styles.wElementSubText}>Learn and use technologies found in industry. Build with a sense of purpose and the knowledge that the work you here do has real potential for positive impact.</div>
-            </div>
-        </div>
-        <div className={styles.wElement}>
-            <div className={styles.wElementImage}>
-            <Image
-                src={onboard}
-                alt="Picture of the author"
-                width={300}
-                height={200}
-                // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
-            />
-            </div>
-            <div className={styles.wElementTextContainer}>
-                <div className={styles.wElementText}>Seamless Project Onboarding</div>
-                <div className={styles.wElementSubText}>Need a team for a project? With our integrated onboarding approaches this becomes a breeze. All the while, you&apos;re helping people learn about the tech in your field.</div>
+
+            <div>
+                <div className={styles.ContentSubHeader}>For Developers</div>
+                <div className={styles.wElement}>
+                    <div className={styles.wElementImage}>
+                    <Image
+                        src={group}
+                        alt="Picture of the author"
+                        width={300} 
+                        height={200}
+                        // blurDataURL="data:..." automatically provided
+                        // placeholder="blur" // Optional blur-up while loading
+                    />
+                    </div>
+                    <div className={styles.wElementTextContainer}>
+                        <div className={styles.wElementText}>Peer-to-peer Networking</div>
+                        <div className={styles.wElementSubText}>Find like-minded individuals in the local community. Engage and collaborate on projects with provided resources. Let's build something together today.</div>
+                    </div>
+                </div>
+                <div className={styles.wElement}>
+                    <div className={styles.wElementImage}>
+                    <Image
+                        src={group}
+                        alt="Picture of the author"
+                        width={300} 
+                        height={200}
+                        // blurDataURL="data:..." automatically provided
+                        // placeholder="blur" // Optional blur-up while loading
+                    />
+                    </div>
+                    <div className={styles.wElementTextContainer}>
+                        <div className={styles.wElementText}>Community Projects</div>
+                        <div className={styles.wElementSubText}>Learn and use technologies found in industry. Build with a sense of purpose and the knowledge that the work you here do has real potential for positive impact.</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
